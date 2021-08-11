438b096633d8a306010a80a578e890b06c7ffefb
Marcio Woitek <woitek@usp.br>
2021-08-11 13:23:51 -0300

    Add store read endpoint

--- section_3/app.py
+++ section_3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -27,7 +27,7 @@ def create_item_in_store(name):
 
 @app.route("/store")
 def get_stores():
-    pass
+    return jsonify({"stores": stores})
 
 
 @app.route("/store/<string:name>")
