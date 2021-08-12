963d92c1f0cef4599730bfb73c1c9d622e7f8422
Marcio Woitek <woitek@usp.br>
2021-08-12 17:29:04 -0300

    Improved get and post methods of Item class
    - Improved authentication code
    - Add data validation

--- section_4/app.py
+++ section_4/app.py
@@ -6,13 +6,21 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import jwt_required
 from flask_restful import Api
 from flask_restful import Resource
+from flask_restful import reqparse
 from security import authenticate
 from security import get_current_user
 
 app = Flask(__name__)
 
+# Setup Flask-JWT-Extended
 app.config["JWT_SECRET_KEY"] = "my_secret_key"
 jwt = JWTManager(app)
+
+
+@jwt.expired_token_loader
+def my_expired_token_callback(jwt_header, jwt_payload):
+    return jsonify({"error": "Token has expired"}), 401
+
 
 api = Api(app)
 
@@ -40,12 +48,14 @@ def login():
 
     user = authenticate(username, password)
     if user is None:
-        return jsonify({"error": "Bad username or password"}), 401
+        return jsonify({"error": "Unable to login"}), 401
 
     access_token = create_access_token(identity=user.id)
     return jsonify({"access_token": access_token})
 
 
+# This route is not really necessary. Its only purpose is to show how we can
+# get the identity of an authenticated user.
 @app.route("/protected")
 @jwt_required()
 def protected():
@@ -59,19 +69,35 @@ def protected():
 
 
 class Item(Resource):
+    @staticmethod
+    def find_item_by_name(name):
+        return next(filter(lambda x: x["name"] == name, items), None)
+
     def get(self, name):
-        item = next(filter(lambda x: x["name"] == name, items), None)
-        return {"item": item}, 200 if item else 404
-
-    def post(self, name):
-        item = next(filter(lambda x: x["name"] == name, items), None)
+        item = Item.find_item_by_name(name)
         if item:
-            return {"error": f"An item with name {name} already exists"}, 400
+            return {"item": item}
+        return {"error": f"Item named {name} does not exist"}, 404
 
-        request_data = request.get_json()
+    @jwt_required()
+    def post(self, name):
+        item = Item.find_item_by_name(name)
+        if item:
+            return {"error": f"An item named {name} already exists"}, 400
+
+        # Validate request data
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            "price",
+            type=float,
+            required=True,
+            help="Unable to create item with no price",
+        )
+        args = parser.parse_args(strict=True)
+
         item = {
             "name": name,
-            "price": request_data["price"],
+            "price": args["price"],
         }
         items.append(item)
         return item, 201
@@ -83,7 +109,10 @@ class Item(Resource):
 
         item_idx = items.index(item)
         items.pop(item_idx)
-        return {"message": "Item successfully removed", "item": item}
+        return {
+            "message": "Item successfully removed",
+            "item": item,
+        }
 
     def put(self, name):
         request_data = request.get_json()
@@ -98,7 +127,10 @@ class Item(Resource):
             return new_item, 201
 
         item.update(request_data)
-        return {"message": "Item successfully updated", "item": item}
+        return {
+            "message": "Item successfully updated",
+            "item": item,
+        }
 
 
 class ItemList(Resource):
