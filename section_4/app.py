ff34279c3cc3e2cb158d024e68b04d63c14659fb
Marcio Woitek <woitek@usp.br>
2021-08-11 21:43:02 -0300

    First Flask-RESTful App

---
+++ section_4/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Api
+from flask_restful import Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Student(Resource):
+    def get(self, name):
+        return {"student": name}
+
+
+api.add_resource(Student, "/student/<string:name>")
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
