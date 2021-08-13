d204dd56ef877ccb8fb1e421bfc942758d5e1994
Marcio Woitek <woitek@usp.br>
2021-08-13 19:18:58 -0300

    Add bash scripts for editing and running code

---
+++ section_5/scripts/environment.sh
@@ -0,0 +1,3 @@
+export EDITOR=nvim
+export PYTHONPATH=/home/woitek/repos/udemy_rest_apis_flask/section_5/src/resources:$PYTHONPATH
+export python_virtualenv="/home/woitek/python_envs/udemy_flask/bin/activate"
---
+++ section_5/scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+script_path=$(realpath $0)
+script_dir=$(dirname $script_path)
+source "${script_dir}/environment.sh"
+
+source $python_virtualenv
+
+root_dir=$(dirname $script_dir)
+cd $root_dir
+
+python_script_path="${root_dir}/src/app.py"
+python3 $python_script_path
