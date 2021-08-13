a5fb07260375b13c8f82b4e20534f749b5356a72
Marcio Woitek <woitek@usp.br>
2021-08-13 13:10:29 -0300

    Implement user login using a SQLite database

--- section_5/security.py
+++ section_5/security.py
@@ -1,17 +1,9 @@
 from flask_jwt_extended import get_jwt_identity
 from user import User
 
-users = [
-    User(1, "alice", "asdf"),
-    User(2, "bob", "qwerty"),
-]
-
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
-
 
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     if user and user.password == password:
         return user
     return None
@@ -19,4 +11,4 @@ def authenticate(username, password):
 
 def get_current_user():
     user_id = get_jwt_identity()
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
--- section_5/user.py
+++ section_5/user.py
@@ -1,5 +1,38 @@
+import sqlite3
+
+DB_PATH = "data.sqlite"
+
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+
+    @classmethod
+    def find_by_username(cls, username):
+        conn = sqlite3.connect(DB_PATH)
+        cur = conn.cursor()
+
+        query = "SELECT * FROM users WHERE username = ?"
+        query_results = cur.execute(query, (username,))
+
+        result = query_results.fetchone()
+        user = cls(*result) if result else None
+
+        conn.close()
+        return user
+
+    @classmethod
+    def find_by_id(cls, _id):
+        conn = sqlite3.connect(DB_PATH)
+        cur = conn.cursor()
+
+        query = "SELECT * FROM users WHERE id = ?"
+        query_results = cur.execute(query, (_id,))
+
+        result = query_results.fetchone()
+        user = cls(*result) if result else None
+
+        conn.close()
+        return user
