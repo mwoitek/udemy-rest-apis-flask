39412f42a29ff726db9f48a6df94e2dcf5f686ff
Marcio Woitek <woitek@usp.br>
2021-08-14 00:25:27 -0300

    User registration: Enforce uniqueness of username

--- section_5/src/resources/user_register.py
+++ section_5/src/resources/user_register.py
@@ -2,6 +2,7 @@ import sqlite3
 from pathlib import Path
 from flask_restful import Resource
 from flask_restful import reqparse
+from user import User
 
 DB_PATH = str(Path(__file__).resolve().parents[2] / "data.sqlite")
 
@@ -22,12 +23,18 @@ class UserRegister(Resource):
     )
 
     def post(self):
+        args = UserRegister.parser.parse_args(strict=True)
+        username = args["username"]
+
+        user = User.find_by_username(username)
+        if user:
+            return {"error": f"An user with username {username} already exists"}, 400
+
         conn = sqlite3.connect(DB_PATH)
         cur = conn.cursor()
 
         sql_insert = "INSERT INTO users (username, password) VALUES (?, ?)"
-        args = UserRegister.parser.parse_args(strict=True)
-        cur.execute(sql_insert, (args["username"], args["password"]))
+        cur.execute(sql_insert, (username, args["password"]))
 
         conn.commit()
         conn.close()
